3bb2c9c6bbd778edb546b94e1adcadc0a3182eaf
arman <fakidarman@gmail.com>
2024-06-04 10:22:34 -0400

    using assert to check the return value

--- test/helloTesting.js
+++ test/helloTesting.js
@@ -4,6 +4,7 @@ contract("Hello Testing",()=>{
     it("should return hello testing",async()=>{
         const helloTesting = await HelloTesting.deployed();
         const result = await helloTesting.print();
-        console.log("It prints "+result);
+        // console.log("It prints "+result);
+        assert(result === "Hello Testing");
     })
 })
